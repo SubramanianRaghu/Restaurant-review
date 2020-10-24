8082d8ee1f96ccd579b46428f0b6dfe6c2f6f607
SubramanianRaghu <66899358+SubramanianRaghu@users.noreply.github.com>
2020-10-24 20:13:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request
+import pickle
+
+filename = 'model_moviewreview.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+filename1 = 'cv-transform.pkl'
+cv = pickle.load(open(filename1, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = classifier.predict(vect)
+        return render_template('result.html', prediction = my_prediction)
+    
+#if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=8080)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
